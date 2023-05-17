33b635c048c41cbc309b87033150e52b7b8ff801
andersonbosa <andersonbosa0@gmail.com>
2023-05-16 21:51:29 -0300

    add users endpoint

--- services/time-capsule/src/index.ts
+++ services/time-capsule/src/index.ts
@@ -1,13 +1,20 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 
 const serverInstance = fastify()
+const prisma = new PrismaClient()
 
 
 serverInstance.get('/', (_request, reply) => {
   reply.send('I want to fuck, Sarah!')
 })
 
+
+serverInstance.get('/users', async (request, reply) => {
+  const users = await prisma.user.findMany()
+  reply.send(users)
+})
 
 serverInstance.listen({ port: 3000 })
   .then(() => {
