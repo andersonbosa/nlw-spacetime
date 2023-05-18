018b4c7477a778f334b9bfa822d3c80be7cd0a39
andersonbosa <andersonbosa0@gmail.com>
2023-05-17 23:36:12 -0300

    checkpoint

---
+++ services/mobile-nlw-spacetime/App.jsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+
+export default function App () {
+  return (
+    <View className="bg-gray-950 flex-1 items-center justify-center">
+      <Text className='text-zinc-50 font-bold text-5xl'>Hello world!</Text>
+      <StatusBar style="light" />
+    </View>
+  )
+}
+
+const _styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
