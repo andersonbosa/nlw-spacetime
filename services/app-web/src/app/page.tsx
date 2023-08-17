1b30b13d6eddb06a9aef4955906f45fbd1e28d24
andersonbosa <andersonbosa0@gmail.com>
2023-08-17 11:07:53 -0300

    Checkpoint

--- services/app-web/src/app/page.tsx
+++ services/app-web/src/app/page.tsx
@@ -16,38 +16,33 @@ const mountMemories = (memories: IMemoryProps[]) => {
 // TOFIX: problem when FC or Promise<JSX.Element>
 const Home: any = async () => {
   const isAuthenticated = cookies().has('token')
-  let view
 
   if (!isAuthenticated) {
-    view = <EmptyMemories />
-  } else {
-    const token = cookies().get('token')?.value
-
-    try {
-      const response = await api.get('/memories', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-
-      const memories: IMemoryProps[] = response.data
-
-      if (memories.length === 0) {
-        view = <EmptyMemories />
-      } else {
-        view = (
-          <div className="flex flex-col gap-10 p-8">
-            {mountMemories(memories)}
-          </div>
-        )
-      }
-    } catch (error) {
-      console.error('Error fetching memories:', error)
-      view = <div>Error fetching memories</div>
-    }
+    return <EmptyMemories />
   }
 
-  return <div className="container mx-auto">{view}</div>
+  try {
+    const token = cookies().get('token')?.value
+
+    const response = await api.get('/memories', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    const memories: IMemoryProps[] = response.data
+
+    if (memories.length === 0) {
+      return <EmptyMemories />
+    }
+
+    return (
+      <div className="flex flex-col gap-10 p-8">{mountMemories(memories)}</div>
+    )
+  } catch (error) {
+    console.error('Error fetching memories:', error)
+    return <div>Error fetching memories</div>
+  }
 }
 
 export default Home
